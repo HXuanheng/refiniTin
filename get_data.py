4220654f8943c7aebca3439128dba03d99448a77
Xuanheng Huang <75201629+HXuanheng@users.noreply.github.com>
2023-07-06 15:11:07 +0200

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -13,28 +13,21 @@ def clean_filename(filename):
 
 
 #TO_CSV
-to_csv=dict(sep=',', na_rep='.', float_format=None, columns=None, header=True, index=True, index_label=None, mode='w', encoding="utf-8-sig", compression='infer', quoting=csv.QUOTE_ALL, quotechar='"', chunksize=10000, date_format=None, doublequote=True, escapechar='\\', decimal='.', errors='strict')
+to_csv=dict(sep=',', na_rep='.', float_format=None, columns=None, header=True, index=False, index_label=None, mode='w', encoding="utf-8-sig", compression='infer', quoting=csv.QUOTE_ALL, quotechar='"', chunksize=10000, date_format=None, doublequote=True, escapechar='\\', decimal='.', errors='strict')
 #READ_CSV
 read_csv=dict(sep=',', delimiter=None, header='infer', names=None, index_col=None, usecols=None, dtype=str, engine=None, converters=None, true_values=None, false_values=None, skipinitialspace=False, skiprows=None, skipfooter=0, nrows=None, na_values=None, keep_default_na=True, na_filter=True, verbose=False, skip_blank_lines=True, keep_date_col=False, dayfirst=False, cache_dates=True, iterator=False, chunksize=None, compression='infer', thousands=None, decimal='.', lineterminator=None, quotechar='"', quoting=csv.QUOTE_ALL, doublequote=True, escapechar='\\', comment=None, encoding=None, dialect=None, delim_whitespace=False, low_memory=True, memory_map=False, float_precision=None)
 
 #PRELIMINARY
 ek.set_app_key(appkey)
 
-def get_data():
-    frames=None
-    item="instruments"
-    #COLUMN -CHANGE
-    instruments=pd.read_csv(resources+item+".csv", **read_csv)[item].tolist()
-    #FIELDS
-    item="fields"
-    fields=pd.read_csv(resources+item+".csv", **read_csv)[item].tolist()
-    #PARAMETERS
-    item="parameters"
-    parameters=pd.read_csv(resources+item+".csv", **read_csv)[item].tolist()
-    frames=[None]*len(instruments)*len(parameters)
-    i=0
+
+def read_data(item):
+    return pd.read_csv(resources+item+".csv", **read_csv)[item].tolist()
+
+def get_data(instruments,chunkname):
+    fields = read_data('fields')
+    parameters = read_data('parameters')
     for parameter in parameters:
-            # filename = parameter[-8:-2]
             filename = re.findall(r'"([^"]*)"',parameter)
             filename = filename[-1]
             try:
@@ -45,11 +38,11 @@ def get_data():
                                    raw_output=False, 
                                    debug=False)
                 df['parameter'] = filename
-                df.to_csv(results+'/to_append/'+filename+'.csv', **to_csv)
+                df.to_csv(results+'/to_append/'+filename+'_'+chunkname+'.csv', **to_csv)
                 print('Parameter '+parameter+' completed...')
             except:
                 # Raise a warning instead of an exception
-                warnings.warn('Parameter '+filename+' raise warning!!!', RuntimeWarning)
+                warnings.warn('Parameter '+filename+'_'+chunkname+' raise warning!!!', RuntimeWarning)
 
 def append_data():
     all_files = os.listdir(results+'/to_append/')
@@ -63,10 +56,31 @@ def append_data():
         
     concatenated_df = pd.concat(df_list, axis=0)
     concatenated_df.to_csv(results+'data_appended.csv', index=False)
-    # data=pd.concat(frames).reset_index(drop=True)
-    # data.to_csv(results+"data_output.csv", **to_csv)
 
-if __name__ == "__main__":
-    get_data()
+def main():
+    instruments = read_data('instruments')
+    # Determine the size of each chunk
+    chunk_size = 2000
+
+    # Calculate the number of chunks
+    total_instruments = len(instruments)
+    num_chunks = (total_instruments + chunk_size - 1) // chunk_size
+
+    # Iterate over the range of chunk numbers
+    for chunk_number in range(num_chunks):
+        # Determine the start and end indices for the current chunk
+        start_index = chunk_number * chunk_size
+        end_index = min(start_index + chunk_size, total_instruments)
+        
+        # Extract the chunk from the instruments vector
+        chunk = instruments[start_index:end_index]
+
+        # Call the get_data function with the current chunk and chunk number
+        get_data(chunk, str(chunk_number))
+
+    # Append data    
     append_data()
     print("Process Completed...")
+
+if __name__ == "__main__":
+    main()
